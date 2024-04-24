b4110a3b2b1bce7f56a927c5468444d2877b5893
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-04-24 14:36:38 +0200

    Added VScode to gitignore
    ---------
    
    Co-authored-by: Fabian Kirchner <kirchner@users.noreply.github.com>

--- src/ontopint/__init__.py
+++ src/ontopint/__init__.py
@@ -4,6 +4,7 @@ from pint import UnitRegistry
 
 ureg = UnitRegistry()
 
+
 class UnitDecoder(json.JSONDecoder):
     def __init__(self, *args, **kwargs):
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
@@ -12,14 +13,18 @@ class UnitDecoder(json.JSONDecoder):
         if 'unit' in obj and 'value' in obj:
             return ureg.Quantity(obj['value'], obj['unit'])
         unit = None
-        unit_defs = [v for k, v in obj.items() if isinstance(v, str) and v.startswith('qudt:')]
+        unit_defs = [
+            v for k, v in obj.items() if isinstance(v, str) and v.startswith('qudt:')
+        ]
         if len(unit_defs) > 1:
             raise ValueError('More that one unit definition')
         if unit_defs:
             iri = unit_defs[0].replace('qudt:', 'http://qudt.org/vocab/unit/')
             graph = rdflib.Graph()
             graph.parse(iri)
-            result = graph.query(f"SELECT * WHERE {{<{iri}> <http://qudt.org/schema/qudt/symbol> ?symbol}}")
+            result = graph.query(
+                f'SELECT * WHERE {{<{iri}> <http://qudt.org/schema/qudt/symbol> ?symbol}}'
+            )
             unit = result.bindings[0]['symbol']
             for k, v in obj.items():
                 if isinstance(v, float):
